8bd8da764656f80fa359950787b5704cc3c41bc0
Vadim <mpcomp@gmail.com>
2026-02-11 13:20:14 +0300

    chore: update brands and .gitignore, exclude backups

--- web/src/utils/brands.ts
+++ web/src/utils/brands.ts
@@ -5,6 +5,12 @@ export const brandConfig: Record<string, { logo?: string, color?: string }> = {
     'chigo': { logo: '/img/logos/chigo.svg', color: 'text-orange-500' },
     'hisense': { logo: '/img/logos/hisense.svg', color: 'text-teal-600' },
     'aux': { logo: '/img/logos/aux.svg', color: 'text-red-600' },
+    'lg': { logo: '/img/logos/lg.svg', color: 'text-blue-600' },
+    'mitsubishi': { logo: '/img/logos/mitsubishi.svg', color: 'text-red-600' },
+    'daikin': { logo: '/img/logos/daikin.svg', color: 'text-blue-600' },
+    'gree': { logo: '/img/logos/gree.svg', color: 'text-green-600' },
+    'electrolux': { logo: '/img/logos/electrolux.svg', color: 'text-blue-600' },
+    'ballu': { logo: '/img/logos/ballu.svg', color: 'text-red-600' },
 };
 
 export const getBrandConfig = (slug: string) => {
