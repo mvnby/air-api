229bb402121ad0539d794f5ef4cffd06369f2f5a
Vadim <mpcomp@gmail.com>
2026-02-20 14:56:10 +0300

    fix(ci): regenerate openapi.json and TypeScript client after import-onliner endpoint

---
+++ manager_frontend/src/client/models/OnlinerImportResultResponse.ts
@@ -0,0 +1,11 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type OnlinerImportResultResponse = {
+    success_count: number;
+    error_count: number;
+    successes: Array<string>;
+    errors: Array<string>;
+};
+
