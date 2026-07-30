5da3773f6358509df76bcdcd4dba8da2c9a09d9f
Vadim <mpcomp@gmail.com>
2026-07-30 07:26:53 +0300

    test: update Alembic head assertions

--- tests/unit/test_order_source_fingerprint_migration.py
+++ tests/unit/test_order_source_fingerprint_migration.py
@@ -30,7 +30,7 @@ def test_order_source_fingerprint_remains_in_the_single_alembic_chain():
     script = ScriptDirectory.from_config(Config("alembic.ini"))
     revision = script.get_revision(REVISION)
 
-    assert script.get_heads() == ["f6b2a4d8e1c3"]
+    assert script.get_heads() == ["a7c8d9e0f1b2"]
     assert revision is not None
     assert revision.down_revision == "f3c4d5e6f7a8"
     assert set(revision.nextrev) == {"d8e7f6a5b4c3"}
